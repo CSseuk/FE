52d94a60b183acdaa3e2760a65be4cfe4bd5bd01
jungsun <dlwjdtjs0111@gmail.com>
2025-10-01 18:11:37 +0900

    feat: InputLabel component 구현

---
+++ src/storybook/stories/design-system/components/InputLabel/InputLabel.styles.ts
@@ -0,0 +1,24 @@
+import styled from "@emotion/native";
+
+export const InputLabelContainer = styled.View`
+  gap: 8px;
+  width: 100%;
+  align-items: flex-start;
+`;
+
+export const InputWrapper = styled.View`
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.Neutral.N0};
+`;
+
+export const Label = styled.Text(({ theme }) => ({
+  ...theme.typography.Body1,
+  color: theme.colors.Neutral.N600,
+}));
+
+export const Caption = styled.Text(({ theme }) => ({
+  ...theme.typography.Body3,
+  color: theme.colors.Neutral.N200,
+}));
