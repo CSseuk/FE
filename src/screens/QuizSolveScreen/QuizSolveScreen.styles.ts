a4a876a54c07456b8b1d505a18262b46b34c5450
taejun0 <ohtaetaejun@gmail.com>
2025-10-14 12:53:37 +0900

    [Fix] #25 - 뒷 배경 색상 오류 해결

--- src/screens/QuizSolveScreen/QuizSolveScreen.styles.ts
+++ src/screens/QuizSolveScreen/QuizSolveScreen.styles.ts
@@ -1,11 +1,13 @@
 import styled from '@emotion/native';
 
-export const Wrapper = styled.View(() => ({
+export const Wrapper = styled.View(({ theme }) => ({
   position: 'relative',
   flex: 1,
+
+  backgroundColor: theme.colors.Neutral.N0,
 }));
 
-export const Container = styled.View(() => ({
+export const Container = styled.View(({}) => ({
   display: 'flex',
   flexDirection: 'column',
   padding: 24,
