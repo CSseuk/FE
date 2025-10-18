f725865b3df7edada9f407c27cd4f38ed751bc7d
jungsun <dlwjdtjs0111@gmail.com>
2025-10-18 23:36:55 +0900

    fix: Screen 이름 수정

--- src/screens/SigninScreen/index.tsx
+++ src/screens/SigninScreen/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-export default function LoginScreen() {
+export default function SigninScreen() {
   return (
     <View
       style={{
